0f0f0267d3b9ae6685edacb3cb9ae2992ea638f5
Sam Stockstrom <144393153+sstock2005@users.noreply.github.com>
2024-12-03 18:39:49 -0600

    Quartz sync: Dec 3, 2024, 6:39 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -19,7 +19,7 @@ const config: QuartzConfig = {
     baseUrl: "blog.sstock.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
-    generateSocialImages: false,
+    generateSocialImages: true,
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
